628c307adff69d11e550871a9e0e4454ddefd368
Avinash Vyas <avinash.vyas@nokia.com>
2018-07-16 12:53:54 -0400

    first commit for rasa robot controller

--- __init__.py
+++ __init__.py
@@ -6,10 +6,45 @@ class RasaRobotControllerSkill(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_handler(IntentBuilder().require('RasaRobotController'))
-    def handle_rasa_robot_controller(self, message):
-        self.speak_dialog('rasa.robot.controller')
+    def initialize(self):
+        self.register_rasa_intent('move.json', handle_robot_move)
+        self.register_rasa_intent('stop.json', handle_robot_stop)
+        self.register_rasa_intent('turn.json', handle_robot_turn)
 
+    def handle_robot_move(self, message):
+        distance = message.data.get("distance")
+        if distance is None:
+            self.speak_dialog('robot.no.distance')
+            return
+        direction = message.data.get("direction")
+        if direction is None:
+            self.speak_dialog('robot.no.direction')
+            return
+        unit = message.data.get("unit")
+        if unit is None:
+            self.speak_dialog('robot.no.unit')
+            return
+        resp = {'distance' : distance, 'direction' : direction, 'unit' : unit}
+        self.speak_dialog('robot.move', data=resp)
+
+    def handle_robot_turn(self, message):
+        distance = message.data.get("distance")
+        if distance is None:
+            self.speak_dialog('robot.no.distance')
+            return
+        direction = message.data.get("direction")
+        if direction is None:
+            self.speak_dialog('robot.no.direction')
+            return
+        unit = message.data.get("unit")
+        if unit is None:
+            self.speak_dialog('robot.no.unit')
+            return
+        resp = {'distance' : distance, 'direction' : direction, 'unit' : unit}
+        self.speak_dialog('robot.move', data=resp)
+
+    def handle_robot_stop(self, message):
+        self.speak_dialog('robot.stop')
 
 def create_skill():
     return RasaRobotControllerSkill()
