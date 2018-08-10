a0889652c72483e6919a5f2032a76b94515e8082
Avinash Vyas <avinash.vyas@nokia.com>
2018-08-10 11:12:34 -0400

    adding intent to update an intent

--- __init__.py
+++ __init__.py
@@ -5,13 +5,28 @@ from mycroft import MycroftSkill, intent_handler
 class RasaRobotControllerSkill(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+        self.utterance = None
 
     def initialize(self):
         self.register_rasa_intent('move.json', self.handle_robot_move)
         self.register_rasa_intent('stop.json', self.handle_robot_stop)
         self.register_rasa_intent('turn.json', self.handle_robot_turn)
+        self.register_rasa_intent('error.json', self.handle_intent_error)
+
+    def handle_intent_error(self, message):
+        if self.utterance is None:
+            self.speak_dialog("This is embarrassing, I could not find the previous utterance")
+            return
+        resp = {'utt': self.utterance, 'intents': "move or turn or fetch"}
+        intent = self.get_response(dialog='robot.query.user', data=resp)
+        if intent is None:
+            intent = self.get_response(dialog='robot.repeat.prompt')
+        resp = {'utt': self.utterance, 'intent' : intent}
+        self.speak_dialog('robot.confirm.intent', data=resp)
 
     def handle_robot_move(self, message):
+        self.utterance = message.data.get("utterance");
+        print "Utterance = " + self.utterance
         distance = message.data.get("distance")
         if distance is None:
             self.speak_dialog('robot.no.distance')
@@ -28,6 +43,8 @@ class RasaRobotControllerSkill(MycroftSkill):
         self.speak_dialog('robot.move', data=resp)
 
     def handle_robot_turn(self, message):
+        self.utterance = message.data.get("utterance");
+        print "Utterance = " + self.utterance
         distance = message.data.get("distance")
         if distance is None:
             self.speak_dialog('robot.no.distance')
